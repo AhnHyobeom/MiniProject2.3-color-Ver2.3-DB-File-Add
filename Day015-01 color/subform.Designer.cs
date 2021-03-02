23b49f533712c7e5348c61b0cebd065e08e8918e
효범 안 <dksgyqja123@naver.com>
2021-03-02 16:07:13 +0900

    초기 커밋.

---
+++ Day015-01 color/subform.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace Day015_01_color
+{
+    partial class subform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numUp_value = new System.Windows.Forms.NumericUpDown();
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numUp_value)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(142, 109);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "입력 : ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // numUp_value
+            // 
+            this.numUp_value.DecimalPlaces = 2;
+            this.numUp_value.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.numUp_value.Location = new System.Drawing.Point(318, 107);
+            this.numUp_value.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numUp_value.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.numUp_value.Minimum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            -2147483648});
+            this.numUp_value.Name = "numUp_value";
+            this.numUp_value.Size = new System.Drawing.Size(150, 39);
+            this.numUp_value.TabIndex = 1;
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_ok.Location = new System.Drawing.Point(170, 215);
+            this.btn_ok.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(94, 43);
+            this.btn_ok.TabIndex = 2;
+            this.btn_ok.Text = "OK";
+            this.btn_ok.UseVisualStyleBackColor = true;
+            this.btn_ok.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_cancel.Location = new System.Drawing.Point(335, 215);
+            this.btn_cancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(94, 43);
+            this.btn_cancel.TabIndex = 3;
+            this.btn_cancel.Text = "cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // subform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 329);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_ok);
+            this.Controls.Add(this.numUp_value);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "subform";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "subform";
+            ((System.ComponentModel.ISupportInitialize)(this.numUp_value)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.NumericUpDown numUp_value;
+        private System.Windows.Forms.Button btn_ok;
+        private System.Windows.Forms.Button btn_cancel;
+    }
+}
